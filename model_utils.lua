ef0c7ad9c4bafa57c2d81e9327294a113f611c49
Lei Jimmy Ba <jimmy@psi.utoronto.ca>
2015-06-16 13:52:31 -0400

    added a slightly faster version of clonemanytimes

--- model_utils.lua
+++ model_utils.lua
@@ -155,4 +155,69 @@ function model_utils.clone_many_times(net, T)
     return clones
 end
 
+-- Authors: Tomas Kocisky
+-- (Modification of above.)
+
+function model_utils.clone_many_times_fast(net, T)
+   local clones = {}
+   local params, gradParams = net:parameters()
+   if params == nil then
+      params = {}
+   end
+   local paramsNoGrad
+   if net.parametersNoGrad then
+      paramsNoGrad = net:parametersNoGrad()
+   end
+   local mem = torch.MemoryFile("w"):binary()
+   mem:writeObject(net)
+
+   -- serialize an empty clone for faster cloning
+   do
+      local reader = torch.MemoryFile(mem:storage(), "r"):binary()
+      local clone = reader:readObject()
+      reader:close()
+      local cloneParams, cloneGradParams = clone:parameters()
+      local cloneParamsNoGrad
+      for i = 1, #params do
+         cloneParams[i]:set(torch.Tensor():typeAs(params[i]))
+         cloneGradParams[i]:set(torch.Tensor():typeAs(gradParams[i]))
+      end
+      if paramsNoGrad then
+         cloneParamsNoGrad = clone:parametersNoGrad()
+         for i =1,#paramsNoGrad do
+            cloneParamsNoGrad[i]:set(torch.Tensor():typeAs(paramsNoGrad[i]))
+         end
+      end
+
+      mem:close()
+      mem = torch.MemoryFile("w"):binary()
+      mem:writeObject(clone)
+   end
+   collectgarbage()
+
+   for t = 1, T do
+      -- We need to use a new reader for each clone.
+      -- We don't want to use the pointers to already read objects.
+      local reader = torch.MemoryFile(mem:storage(), "r"):binary()
+      local clone = reader:readObject()
+      reader:close()
+      local cloneParams, cloneGradParams = clone:parameters()
+      local cloneParamsNoGrad
+      for i = 1, #params do
+         cloneParams[i]:set(params[i])
+         cloneGradParams[i]:set(gradParams[i])
+      end
+      if paramsNoGrad then
+         cloneParamsNoGrad = clone:parametersNoGrad()
+         for i =1,#paramsNoGrad do
+            cloneParamsNoGrad[i]:set(paramsNoGrad[i])
+         end
+      end
+      clones[t] = clone
+      collectgarbage()
+   end
+   mem:close()
+   return clones
+end
+
 return model_utils
